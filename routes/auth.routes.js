33f6b8b680e6064748519797a104022e20cc63e5
MaksimPir <maximalka111@yandex.ru>
2022-02-19 16:39:38 +0400

    new file:   .gitignore 	new file:   app.js 	new file:   config/default.json 	new file:   package-lock.json 	modified:   package.json 	new file:   routes/auth.routes.js 	modified:   templates/index.html

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+const{Router}=require('express')
+const router=Router()
+
+router.post('/register',async(req,res)=>{
+    try{
+
+    }
+    catch(e)
+    {
+        
+    }
+})
+router.post('/login',async(req,res)=>{
+
+})
+module.exports=router
