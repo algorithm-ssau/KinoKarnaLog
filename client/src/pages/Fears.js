6e627ca9f6f441c879f3b2da29410b4485c6f8d4
Fahijo <PetrSpirt@yandex.ru>
2022-04-30 18:46:51 +0400

    Залит фильм "молчание ягнят"

--- client/src/pages/Fears.js
+++ client/src/pages/Fears.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react'
 import {AuthContext} from '../context/AuthContext'
 import {Navbar} from '../Navbar'
 import { Header } from '../Header'
-
+import { Film } from '../Film'
  export const FearsPage = () => {
   
 
@@ -16,7 +16,19 @@ import { Header } from '../Header'
           <div style={{padding: '10px 200px 0px'}}>
           <Header text = "Ужасы"/>
             </div>
-            
+            <ul>
+            <hr></hr>
+  <Film  text_head = "Молчание ягнят" ref_video = "https://youtu.be/9JTNhAOVosk"
+    ref_img = "https://images.wbstatic.net/big/new/8590000/8593703-1.jpg"
+  text_desc ="Психопат похищает и убивает молодых женщин по всему Среднему Западу.
+   ФБР, уверенное, что все преступления совершены одним и тем же человеком, поручает агенту Клариссе Старлинг встретиться 
+   с заключенным-маньяком Ганнибалом Лектером, который мог бы помочь составить психологический портрет убийцы.
+   Сам Лектер отбывает наказание за убийства и каннибализм.
+   Он согласен помочь Клариссе лишь в том случае, если она попотчует его больное воображение подробностями своей личной жизни."/>
+
+
+
+</ul>
           </div>
   )
 }
